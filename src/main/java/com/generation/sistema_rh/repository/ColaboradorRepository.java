c518c7fc53af20cc7500ad54d300a0c2b42e0dc7
luizsantos7 <lhsantos098@gmail.com>
2025-05-29 11:29:01 -0300

    Finalização do CRUD Classe Colaborador

--- src/main/java/com/generation/sistema_rh/repository/ColaboradorRepository.java
+++ src/main/java/com/generation/sistema_rh/repository/ColaboradorRepository.java
@@ -1,6 +1,7 @@
 package com.generation.sistema_rh.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,5 +10,7 @@ import com.generation.sistema_rh.model.Colaborador;
 public interface ColaboradorRepository extends JpaRepository<Colaborador, Long> {
 	
 	List<Colaborador> findAllByNomeContainingIgnoreCase(String nome);
+	
+	Optional<Colaborador> findByEmail(String email);
 
 }
